6a83a19a61938e733eb13c4591aba71b7f3b120e
Oskar <oskar.leonard@hotmail.com>
2019-11-19 10:57:11 +0100

    added modal and progressbar components

--- src/client/pages/homePage/HomePage.js
+++ src/client/pages/homePage/HomePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import MultipleFilesIcon from '@rc-lib-client/components/icons/filesEmptyIcon/FilesEmptyIcon';
-import DropzoneWithPreview from '../../components/dropzones/dropzoneWithPreview/DropzoneWithPreview';
+import ProgressBar from '@rc-lib-client/components/loaders/progressBar/ProgressBar';
 import styles from './homePage.scss';
 
 class HomePage extends React.Component {
@@ -8,12 +7,11 @@ class HomePage extends React.Component {
     return (
       <div className={styles.homeRoute}>
         <h1 className={styles.title}>Home page</h1>
-        <DropzoneWithPreview
-          onImageDrop={this.handleImageDrop}
-          onImageRemove={this.handleImageRemove}
-        >
-          <div>DROP FILES HERE</div>
-        </DropzoneWithPreview>
+        <ProgressBar className={styles.progress} progress={33} />
+        <ProgressBar className={styles.progress} progress={55} />
+        <ProgressBar className={styles.progress} progress={66} />
+        <ProgressBar className={styles.progress} progress={100} />
+        <ProgressBar className={styles.progress} progress={88} />
       </div>
     );
   }

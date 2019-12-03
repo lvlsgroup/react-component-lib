973eabcbebfab32e7b7a31876b450590db933dd2
Oskar <oskar.leonard@hotmail.com>
2019-12-03 10:13:26 +0100

    memoize filecard and crossicon

--- src/client/components/cards/fileCards/fileCard/FileCard.js
+++ src/client/components/cards/fileCards/fileCard/FileCard.js
@@ -5,67 +5,67 @@ import CrossIcon from '@rc-lib-client/components/icons/crossIcon/CrossIcon';
 import DocumentIcon from '@rc-lib-client/components/icons/documentIcon/DocumentIcon';
 import VideoFileIcon from '@rc-lib-client/components/icons/videoFileIcon/VideoFileIcon';
 import AudioFileIcon from '@rc-lib-client/components/icons/audioFileIcon/AudioFileIcon';
-import ProgressBar from '@rc-lib-client/components/loaders/progressBar/ProgressBar';
 import styles from './fileCard.scss';
 
-function FileCard({
-  className,
-  onClick,
-  file,
-  onFileRemove,
-  SlotLeft,
-  SlotUnderImage,
-  SlotBottom,
-}) {
-  const getIcon = (type, style) => {
-    if (type.includes('audio/')) {
-      return <AudioFileIcon style={style} />;
-    } else if (type.includes('video/')) {
-      return <VideoFileIcon style={style} />;
-    } else {
-      return <DocumentIcon style={style} />;
-    }
-  };
+const FileCard = React.memo(
+  ({
+    className,
+    onClick,
+    file,
+    onFileRemove,
+    SlotLeft,
+    SlotUnderImage,
+    SlotBottom,
+  }) => {
+    const getIcon = (type, style) => {
+      if (type.includes('audio/')) {
+        return <AudioFileIcon style={style} />;
+      } else if (type.includes('video/')) {
+        return <VideoFileIcon style={style} />;
+      } else {
+        return <DocumentIcon style={style} />;
+      }
+    };
 
-  const handleFileRemove = () => {
-    onFileRemove(file);
-  };
+    const handleFileRemove = () => {
+      onFileRemove(file);
+    };
 
-  const isImage = file.type.includes('image/');
-  const icon = getIcon(file.type, { fill: 'black', height: '64px' });
+    const isImage = file.type.includes('image/');
+    const icon = getIcon(file.type, { fill: 'black', height: '64px' });
 
-  return (
-    <div className={classNames(styles.fileCard, className)}>
-      <div className={styles.imageContentContainer} onClick={onClick}>
-        {SlotLeft && SlotLeft}
-        <div className={styles.imageAndOptionContainer}>
-          <div className={styles.imageOptions}>
-            <CrossIcon
-              onClick={handleFileRemove}
-              crossColor="white"
-              style={{
-                backgroundColor: 'rgba(46,46,46,0.85)',
-              }}
-            />
-          </div>
-          <div className={styles.imageContainer}>
-            {isImage ? (
-              <img
-                className={styles.image}
-                src={URL.createObjectURL(file)}
-                alt="Dropped image"
+    return (
+      <div className={classNames(styles.fileCard, className)}>
+        <div className={styles.imageContentContainer} onClick={onClick}>
+          {SlotLeft && SlotLeft}
+          <div className={styles.imageAndOptionContainer}>
+            <div className={styles.imageOptions}>
+              <CrossIcon
+                onClick={handleFileRemove}
+                crossColor="white"
+                style={{
+                  backgroundColor: 'rgba(46,46,46,0.85)',
+                }}
               />
-            ) : (
-              <div className={styles.icon}>{icon}</div>
-            )}
+            </div>
+            <div className={styles.imageContainer}>
+              {isImage ? (
+                <MemoizedImage
+                  className={styles.image}
+                  src={URL.createObjectURL(file)}
+                />
+              ) : (
+                <div className={styles.icon}>{icon}</div>
+              )}
+            </div>
+            {SlotUnderImage && SlotUnderImage}
           </div>
-          {SlotUnderImage && SlotUnderImage}
         </div>
+        {SlotBottom && SlotBottom}
       </div>
-      {SlotBottom && SlotBottom}
-    </div>
-  );
-}
+    );
+  }
+);
 
 FileCard.propTypes = {
   className: PropTypes.string,
@@ -92,3 +92,7 @@ FileCard.propTypes = {
 };
 
 export default FileCard;
+
+const MemoizedImage = React.memo(({ className, src }) => {
+  return <img className={className} src={src} alt="Dropped image" />;
+});

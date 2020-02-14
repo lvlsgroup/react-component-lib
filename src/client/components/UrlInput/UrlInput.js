96b485c9e013aed60d16cb8d5c433e86b0ad0463
John Pettersson <john.pettersson@wearelevels.com>
2020-02-14 14:58:00 +0100

    added urlInput component and urlUtils

---
+++ src/client/components/UrlInput/UrlInput.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { useHistory, useLocation } from 'react-router-dom';
+import {
+  getSearchParams,
+  getSearchQueryValue,
+} from '@rc-lib-client/shared/utils/urlUtils/urlUtils';
+import Flex from '@rc-lib-client/components/flex/Flex';
+import styles from './urlInput.scss';
+
+function URLInput({ className, placeholder, searchParam }) {
+  const location = useLocation();
+  const history = useHistory();
+  const currentQuery = getSearchQueryValue(location.search, searchParam) || '';
+
+  function onInputChange(event) {
+    let searchParams;
+    if (event.target.value) {
+      searchParams = getSearchParams(location.search, {
+        addParamsObj: { query: event.target.value },
+      });
+    } else {
+      searchParams = getSearchParams(location.search, {
+        removeParamsArray: [searchParam],
+      });
+    }
+
+    history.push({ search: searchParams });
+  }
+
+  function onClear() {
+    const searchParams = getSearchParams(location, {
+      removeParamsArray: [searchParam],
+    });
+    history.push({ search: searchParams });
+  }
+
+  return (
+    <Flex className={classNames(styles.urlInput, className)} alignCenter>
+      <input
+        placeholder={placeholder}
+        className={styles.input}
+        value={currentQuery}
+        onChange={onInputChange}
+      />
+      {currentQuery && (
+        <button className={styles.btnClear} type={'button'} onClick={onClear}>
+          <svg
+            className={styles.crossIcon}
+            height="30"
+            width="30"
+            viewBox="0 0 30 30"
+          >
+            <path
+              className={styles.cross1}
+              d={'M 2 15 L 28 15'}
+              strokeWidth={'3'}
+            />
+            <path
+              className={styles.cross2}
+              d={'M 2 15 L 28 15'}
+              strokeWidth={'3'}
+            />
+          </svg>
+        </button>
+      )}
+    </Flex>
+  );
+}
+
+URLInput.propTypes = {
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
+  searchParam: PropTypes.string,
+};
+
+export default React.memo(URLInput);
---
+++ src/client/shared/utils/urlUtils/urlUtils.js
@@ -0,0 +1,79 @@
+import urlParse from 'url-parse';
+import {
+  getSortedObject,
+  toggleArrayValue,
+} from '@rc-lib-client/shared/utils/jsUtils/jsUtils';
+
+export function getSearchParams(
+  location,
+  { removeParamsArray, addParamsObj } = {
+    removeParamsArray: [],
+    addParamsObj: {},
+  }
+) {
+  const parsedLocationSearch = urlParse(location.search, true);
+
+  const search = Object.assign({}, parsedLocationSearch.query, {
+    ...addParamsObj,
+  });
+
+  removeParamsArray &&
+    removeParamsArray.forEach((item) => {
+      delete search[item];
+    });
+
+  const sortedSearch = getSortedObject(search);
+
+  let searchParamsString = '';
+  let index = 0;
+  for (const [key, value] of Object.entries(sortedSearch)) {
+    const paramDeliminator = index === 0 ? '?' : '&';
+    searchParamsString += value ? `${paramDeliminator}${key}=${value}` : '';
+    index++;
+  }
+
+  return searchParamsString;
+}
+
+export function getSearchQueryValue(search, queryName) {
+  const parsedLocationSearch = urlParse(search, true);
+
+  const queryString = parsedLocationSearch.query
+    ? parsedLocationSearch.query[queryName]
+    : '';
+  return queryString;
+}
+
+export function getToggledSearchQuery(toggleValue, search, queryName) {
+  const queryString = getSearchQueryValue(search, queryName);
+
+  const toggledQueryArray = toggleArrayValue(
+    queryString.split(','),
+    toggleValue
+  );
+
+  return toggledQueryArray && toggledQueryArray.join(',');
+}
+
+export function toggleSearchQuery(history, toggleValue, search, queryName) {
+  const searchQueryValue = getToggledSearchQuery(
+    toggleValue,
+    search,
+    queryName
+  );
+
+  let searchTest = {};
+  if (searchQueryValue && searchQueryValue.length > 0) {
+    searchTest = getSearchParams(location, {
+      addParamsObj: { [queryName]: searchQueryValue },
+    });
+  } else {
+    searchTest = getSearchParams(location, {
+      removeParamsArray: [queryName],
+    });
+  }
+
+  history.push({
+    search: searchTest,
+  });
+}
